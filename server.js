5e36f94f770d2c2ac887469aeec62137fe0729e4
Dmitriy Esko <Dmitriy.esko@gmail.com>
2021-02-23 15:35:57 +0300

    edit pubplicPath on server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public');
+// const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = __dirname;
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
